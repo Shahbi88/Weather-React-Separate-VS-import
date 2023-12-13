d638bb61d7fbc5ab23880c7ba585d0731de8e0d3
Shahbi88 <rihabazar@gmail.com>
2023-12-13 19:45:04 +0000

    Github update link

--- src/footer.js
+++ src/footer.js
@@ -4,7 +4,10 @@ import "./styles.css";
 export default function Footer() {
   return (
     <h6 className="footer">
-      <a href="https://github.com/Shahbi88/Forecast" target="_blank">
+      <a
+        href="https://github.com/Shahbi88/Forecasthttps://github.com/Shahbi88/Weather-React-Separate-VS-import"
+        target="_blank"
+      >
         {" "}
         Open-source code{" "}
       </a>
