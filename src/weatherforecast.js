ce72d5288bcbf1f38f6ecf94502c00dc1269f5eb
Shahbi88 <rihabazar@gmail.com>
2023-12-15 20:23:37 +0000

    attempt forecast call

--- src/weatherforecast.js
+++ src/weatherforecast.js
@@ -1,58 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
+  let [forecast, setForecast] = useState("");
   function handleResponse(response) {
-    console.log(response.data.coord);
-    console.log(response.data.coord.lon);
-    console.log(response.data.coord.lat);
     let longitude = response.data.coord.lon;
     let latitude = response.data.coord.lat;
     let apiKey = "667d9f573c8af4c33457be5d561a9148";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
-  }
+    console.log(response.data.coord);
+    console.log(response.data.coord.lat);
+    setForecast(response.data.daily);
 
-  return (
-    <div className="WeatherForecast">
-      <div class="row">
-        <div class="col">
-          <div className="Day-of-week">Thu</div>
-          <div className="Forecast-icon">🌤️</div>
-          <div>
-            <span className="earlier-temperature">19</span>/10
-          </div>
-        </div>
-        <div class="col">
-          <div className="Day-of-week">Thu</div>
-          <div className="Forecast-icon">🌤️</div>
-          <div>
-            <span className="earlier-temperature">19</span>/10
-          </div>
-        </div>
-        <div class="col">
-          <div className="Day-of-week">Thu</div>
-          <div className="Forecast-icon">🌤️</div>
-          <div>
-            <span className="earlier-temperature">19</span>/10
-          </div>
-        </div>
-        <div class="col">
-          <div className="Day-of-week">Thu</div>
-          <div className="Forecast-icon">🌤️</div>
-          <div>
-            <span className="earlier-temperature">19</span>/10
-          </div>
-        </div>
-        <div class="col">
-          <div className="Day-of-week">Thu</div>
-          <div className="Forecast-icon">🌤️</div>
-          <div>
-            <span className="earlier-temperature">19</span>/10
+    return (
+      <div className="WeatherForecast">
+        <div class="row">
+          <div class="col">
+            <div className="Day-of-week">Thu</div>
+            <div className="Forecast-icon">🌤️</div>
+            <div>
+              <span className="earlier-temperature">
+                {forecast[0].temp.max}19
+              </span>
+              /10
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
