25d13a2ddfc1018070c3d51c619306f6e313e3d2
Shahbi88 <rihabazar@gmail.com>
2023-12-17 18:54:27 +0000

    mobile-friendly at last

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import Header from "./header";
 import Search from "./search";
 import Footer from "./footer";
+import "./styles.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
